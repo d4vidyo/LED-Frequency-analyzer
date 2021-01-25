dbea5005ea11d4a1e74a9e994d299725e0833863
d4vidyo <d4vidyo@users.noreply.github.com>
2021-01-25 19:33:28 +0100

    Update ESP32_Frequency-Analyser_WIP.ino
    Upgradet the function which determines which FFT result gets mapped to which LED. This also applies to the LED color.
    This has also been added to the Mapping_frequency_to_Buckets.ino code
    
    Cleant up the void setup() function by moving the code into seperate functions.
    Added some comments to the variable declarations at the top.

--- Additional Code/Mapping_frequency_to_Bucket/Mapping_frequency_to_Bucket.ino
+++ Additional Code/Mapping_frequency_to_Bucket/Mapping_frequency_to_Bucket.ino
@@ -5,26 +5,96 @@ void setup() {
 }
 
 void loop() {
-  int BucketFrequency[64];
-  int startingFrequency = 20;
-  int delta = 7;
-  for (int i = 0; i < 64; i++) {
+  /*
+    int BucketFrequency[64];
+    int startingFrequency = 20;
+    int delta = 7;
+    for (int i = 0; i < 64; i++) {
 
-    startingFrequency = startingFrequency + delta;
-    if (i == 0) {
-      BucketFrequency[i] = startingFrequency;
+      startingFrequency = startingFrequency + delta;
+      if (i == 0) {
+        BucketFrequency[i] = startingFrequency;
+      }
+      else {
+        BucketFrequency[i] = BucketFrequency[i - 1] + startingFrequency;
+      }
+    }*/
+#define Zeile 6
+#define Spalte 7
+  double Matrix[Zeile][Spalte] = { {1, 1, 1, 1, 1, 1, 39},
+    {pow(59, 5), pow(59, 4), pow(59, 3), pow(59, 2), 59, 1, 12000},
+    {pow(33, 5), pow(33, 4),  pow(33, 3), pow(33, 2), 33, 1,  4000},
+    {pow(50, 5), pow(50, 4), pow(50, 3), pow(50, 2), 50, 1,  8000},
+    {pow(64, 5), pow(64, 4), pow(64, 3), pow(64, 2), 64, 1, 16000},
+    {80, 32, 12, 4, 1, 0, 39}
+  };
+  double a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
+  float x = 0;
+  int BucketFrequency[64];
+  for (int k = 0; k < Zeile; k++) {
+    for (int j = k + 1; j < Zeile; j++) {
+      a = Matrix[j][k] / Matrix[k][k];
+      for (int i = k; i < Spalte; i++) {
+        Matrix[j][i] = Matrix[j][i] - a * Matrix[k][i];
+      }
     }
-    else {
-      BucketFrequency[i] = BucketFrequency[i - 1] + startingFrequency;
+    for (int ii = 0; ii < Zeile; ii++) {
+      for (int jj = 0; jj < Spalte; jj++) {
+        Matrix[ii][jj] /= 10;
+      }
     }
   }
+  f = Matrix[5][6] / Matrix[5][5];
+  e = (Matrix[4][6] - Matrix[4][5] * f) / Matrix[4][4];
+  d = (Matrix[3][6] - Matrix[3][5] * f - Matrix[3][4] * e ) / Matrix[3][3];
+  c = (Matrix[2][6] - Matrix[2][5] * f - Matrix[2][4] * e - Matrix[2][3] * d) / Matrix[2][2];
+  b = (Matrix[1][6] - Matrix[1][5] * f - Matrix[1][4] * e - Matrix[1][3] * d - Matrix[1][2] * c) / Matrix[1][1];
+  a = (Matrix[0][6] - Matrix[0][5] * f - Matrix[0][4] * e - Matrix[0][3] * d - Matrix[0][2] * c - Matrix[0][1] * b) / Matrix[0][0];
+
+  for (int i = 1; i <= 64; i++) {
+    BucketFrequency[i-1] = (a * pow(i, 5)) + (b * pow(i, 4)) + (c * pow(i, 3)) + (d * pow(i, 2)) + (e * i) + f;
+  }
+
   for (int i = 0; i < 64; i++) {
     Serial.print("Bucket ");
     Serial.print(i);
     Serial.print(" = ");
     Serial.print(BucketFrequency[i]);
-    Serial.println("hz");
+    Serial.print("hz");
+    Serial.println(" , ");
   }
+
+  int Diff[64];
+  int smallest = 0;
+  int smallestPosition[1024];
+  for (uint16_t i = 0; i < 1024 / 2; i++) {
+    double abscissa = ((i * 1.0 * 40000) / 1024);
+
+    for (int j = 0; j < 64; j++) {
+      Diff[j] = abscissa - BucketFrequency[j];
+
+      if (Diff[j] < 0) {
+        Diff[j] = -Diff[j];
+      }
+      if (j == 0) {
+        smallest = Diff[j];
+      }
+      if (smallest >= Diff[j]) {
+        smallest = Diff[j];
+        smallestPosition[i] = j;
+      }
+    }
+    Serial.print("Nr: ");
+    Serial.print(i);
+    Serial.print(" smallestPosition: ");
+    Serial.println(smallestPosition[i]);
+  }
+
+
+
+
+
+
 
   int quarter = BucketFrequency[63] / 4;
   int Rfix[64];
@@ -38,28 +108,30 @@ void loop() {
       Bfix[i] = 0;
       sub[0] = BucketFrequency[i];
     }
-    if (BucketFrequency[i] < 2 * quarter && BucketFrequency[i] > quarter) {
+    else if (BucketFrequency[i] < 2 * quarter && BucketFrequency[i] > quarter) {
       Rfix[i] = 255 - 255 * (BucketFrequency[i] - sub[0]) / quarter;
       Gfix[i] = 255;
       Bfix[i] = 0;
       sub[1] = BucketFrequency[i];
     }
-    if (BucketFrequency[i] < 3 * quarter && BucketFrequency[i] > 2 * quarter) {
+    else if (BucketFrequency[i] < 3 * quarter && BucketFrequency[i] > 2 * quarter) {
       Rfix[i] = 0;
       Gfix[i] = 255;
       Bfix[i] = 255 * (BucketFrequency[i] - sub[1]) / quarter;
       sub[2] = BucketFrequency[i];
     }
-    if (BucketFrequency[i] < 4 * quarter && BucketFrequency[i] > 3 * quarter) {
+    else {
       Rfix[i] = 0;
       Gfix[i] = 255 - 255 * (BucketFrequency[i] - sub[2]) / quarter;
       Bfix[i] = 255;
     }
-    Serial.print(Rfix[i]);
-    Serial.print(" , ");
-    Serial.print(Gfix[i]);
-    Serial.print(" , ");
-    Serial.println(Bfix[i]);
+    if (Rfix[i] < 0) {
+      Rfix[i] = 0;
+    } if (Gfix[i] < 0) {
+      Gfix[i] = 0;
+    } if (Bfix[i] < 0) {
+      Bfix[i] = 0;
+    }
   }
 
   delay(10000);
--- ESP32_Frequency-Analyser_WIP/ESP32_Frequency-Analyser_WIP.ino
+++ ESP32_Frequency-Analyser_WIP/ESP32_Frequency-Analyser_WIP.ino
@@ -1,15 +1,17 @@
 //FFT
 #include "arduinoFFT.h">
-
 arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
+#define fps 60                        //Sets max. fps 
+//will go as fast as it can if set to high
 
 //Neopixel
 #include <FastLED.h>
-
 #define LED_COUNT 64
 #define LED_PIN 2
-
 CRGB leds[LED_COUNT];
+
+//Audio
+#define Audio 34                      //Pin used to sample Audio
 
 /*
   These values can be changed in order to evaluate the functions
@@ -21,50 +23,55 @@ const uint8_t lowestband = 20;
 const uint8_t highestband = samplingFrequency / 2;
 
 
-/*These are the input and output vectors
-  Input vectors receive computed results from FFT*/
+/*FFT variables*/
 double vReal[samples];
 double Real[samples];
 volatile double vRealBuffer[samples];
 volatile double vFFT[samples];
 double Imag[samples];
 
-#define Audio 34
-#define fps 30 //Max fps!
-
 /*Input*/
-float u[samples];
-float Period = 1000000 / samplingFrequency;
+float u;                              //Stores sampling Voltage reading
 
 /* Timing Stuff*/
-long TimingOld[6], TimingNew[6];
-long oldTime, newTime;
+int TimingOld, TimingNew;                     //Framerate controll
+int oldTime, newTime;                         //Sampling Frequency controll
+float Period = 1000000 / samplingFrequency;   //Time betweet each sampling reading in microseconds
 
-/*LED stuff*/
-double Buckets[LED_COUNT];
-double BucketsOld[LED_COUNT];
-float R = 0, G = 0, B = 0;
-int Rfix[LED_COUNT];
-int Gfix[LED_COUNT];
-int Bfix[LED_COUNT];
-int smallestPosition[samples / 2];
-int BucketFrequency[LED_COUNT];
-int Bucketentries[LED_COUNT];
-int Basscount = 0;
+/*Bucket Variables*/
+double Buckets[LED_COUNT];            //Stores the Amplitude of all Frequencys
+double BucketsOld[LED_COUNT];         //Stores the Amplitude of all Frequencys from last frame
+int BucketFrequency[LED_COUNT];       //Stores the Frequency designatet to each Buckets[] entry
+int smallestPosition[samples / 2];    //Stores which Buckets[] variable is closest to vReal[] variable
+int Bucketentries[LED_COUNT];         //Stores how many vReal[] variables are put into one Buckets[] variable
 
-float BucketAmplitude[LED_COUNT];
+                                      //Values which corespond to 100% of a Frequency
+float BucketAmplitude[LED_COUNT] = {6021.28, 6021.28, 5350.83, 4720.76, 4164.83, 3702.91, 3336.08, 3048.91,
+                                    2818.86, 2626.81, 2461.88, 2318.82, 2192.6, 2075.32, 1957.44, 1831.19,
+                                    1693.33, 1545.88, 1394.95, 1248.68, 1115.16, 1000.76, 908.93, 839.61,
+                                    789.76, 754.68, 729.84, 712.02, 699.31, 690.18, 682.77, 674.82, 664.35,
+                                    650.2, 632.08, 610.18, 584.9, 556.88, 527.35, 498.25, 471.98, 450.6,
+                                    434.95, 424.09, 415.55, 406.22, 393.73, 377.3, 358.08, 338.48, 321.24,
+                                    308.32, 300.23, 295.87, 292.92, 288.6, 280.5, 267.15, 248.11, 223.72,
+                                    194.66, 161.74, 125.96, 88.61
+                                   };
+                                 
 /*float BucketNoise[64] = {284.43 , 714.73 , 667.95 , 579.42 , 451.34 , 416.01 , 268.92 , 228.56 , 200.94 , 168.76 , 133.52 , 128.60 , 132.47 , 176.75 , 157.20 , 177.93 , 124.83 , 102.09 , 111.68 , 94.17 ,
                          69.79 , 87.22 , 118.06 , 106.82 , 83.47 , 78.88 , 63.66 , 76.13 , 71.21 , 65.49 , 47.32 , 51.52 , 52.82 , 47.39 , 50.41 , 46.77 , 50.18 , 46.98 , 45.22 , 61.64 , 59.21 , 41.16 ,
                          51.24 , 48.89 , 43.70 , 47.39 , 43.54 , 55.91 , 42.58 , 46.39 , 53.77 , 60.31 , 42.50 , 44.84 , 47.86 , 53.01 , 38.61 , 68.58 , 55.88 , 41.50 , 35.53 , 46.39 , 33.63 , 40.40
                         };*/
 
 
+/*LED stuff*/                         //Stores the fixed color for each LED
+int Rfix[LED_COUNT];
+int Gfix[LED_COUNT];
+int Bfix[LED_COUNT];
 
 
 /*Multicore Computing*/
 TaskHandle_t Task1, Task2;
 float CorePeriod = 1000000 / fps; //time a core has to do its Computation in us
-bool checker[7];
+bool checker;
 
 
 void setup()
@@ -72,102 +79,21 @@ void setup()
 
   FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, LED_COUNT);
   FastLED.setBrightness(255);
+  FastLED.show();
 
   Serial.begin(115200);
   Serial.println("Ready");
 
 
+  /*designating Frequency for LEDs*/
+  //BucketFrequencyManual();
+  BucketFrequencyFunction();
 
-  /*BucketAmplitude Calculation*/
-  for (int i = 0; i < LED_COUNT; i++) {
-    if (i == 0) {
-      BucketAmplitude[i] = 5000;
-    }
-    else {
-      int difference = 4930 * log((i + 1) / i) / log(64);
-      BucketAmplitude[i] =  BucketAmplitude[i - 1] - difference;
-    }
+  /*aligning FFT results to LED Frequency*/
+  vRealtoBucket();
 
-  }
-
-  /*Bucket arrangement*/
-  int Diff[64];
-  int smallest;
-  int startingFrequency = lowestband;
-
-  int delta = 7;
-  for (int i = 0; i < 64; i++) {
-
-    if (i == 0) {
-      BucketFrequency[i] = startingFrequency;
-    }
-    else {
-      BucketFrequency[i] = BucketFrequency[i - 1] + startingFrequency;
-    }
-    startingFrequency = startingFrequency + delta;
-  }
-
-  for (uint16_t i = 0; i < samples / 2; i++)
-  {
-    double abscissa = ((i * 1.0 * samplingFrequency) / samples);
-
-    for (int j = 0; j < 64; j++) {
-      Diff[j] = abscissa - BucketFrequency[j];
-
-      if (Diff[j] < 0) {
-        Diff[j] = -Diff[j];
-      }
-
-      if (j == 0) {
-        smallest = Diff[j];
-      }
-      if (smallest >= Diff[j]) {
-        smallest = Diff[j];
-        smallestPosition[i] = j;
-
-      }
-    }
-  }
-  for (int n = 0; n < LED_COUNT; n++) {
-    for (int i = 0; i < samples / 2; i++) {
-      if (smallestPosition[i] == n) {
-        Bucketentries[n]++;
-      }
-    }
-  }
-
-
-
-  /*LED coloring*/
-  int quarter = BucketFrequency[63] / 4;
-  int sub[3];
-  for (int i = 0; i < 64; i++) {
-    if (BucketFrequency[i] < quarter) {
-      Rfix[i] = 255;
-      Gfix[i] = 255 * BucketFrequency[i] / quarter;
-      Bfix[i] = 0;
-      sub[0] = BucketFrequency[i];
-    }
-    if (BucketFrequency[i] < 2 * quarter && BucketFrequency[i] > quarter) {
-      Rfix[i] = 255 - 255 * (BucketFrequency[i] - sub[0]) / quarter;
-      Gfix[i] = 255;
-      Bfix[i] = 0;
-      sub[1] = BucketFrequency[i];
-    }
-    if (BucketFrequency[i] < 3 * quarter && BucketFrequency[i] > 2 * quarter) {
-      Rfix[i] = 0;
-      Gfix[i] = 255;
-      Bfix[i] = 255 * (BucketFrequency[i] - sub[1]) / quarter;
-      sub[2] = BucketFrequency[i];
-    }
-    if (BucketFrequency[i] < 4 * quarter && BucketFrequency[i] > 3 * quarter) {
-      Rfix[i] = 0;
-      Gfix[i] = 255 - 225 * (BucketFrequency[i] - sub[2]) / quarter;
-      Bfix[i] = 255;
-    }
-  }
-
-
+  /*Setting LED colors to corresponding Frequency*/
+  LEDColor();
 
   /*Multicore Computing*/
   xTaskCreatePinnedToCore(
@@ -197,10 +123,10 @@ void CodeCore0(void * parameter ) {
   for (;;) {
 
     //waiting for Core 1
-    while (checker[6]) {
-      TimingOld[0] = micros();
+    while (checker) {
+      yield();
     }
-    checker[6] = 1;
+    checker = 1;
 
     //Buckets ~800us
     startbuckets();
@@ -211,7 +137,6 @@ void CodeCore0(void * parameter ) {
     //Build raw data ~26600us
     startsampling();
 
-    TimingNew[0] = micros();
   }
 }
 
@@ -220,7 +145,7 @@ void CodeCore1(void * parameter ) {
   // Code needs ~30850us
   for (;;) {
 
-    TimingOld[1] = micros();
+    TimingOld = micros();
 
     //Getting Data from Buffer
     for (uint16_t i = 0; i < samples; i++) {
@@ -228,7 +153,7 @@ void CodeCore1(void * parameter ) {
       Imag[i] = 0;
     }
 
-    //Performing FFT ~33400us
+    //Performing FFT ~30700us
     {
       //DCRemoval ~700us
       FFT.DCRemoval(Real, samples);
@@ -242,7 +167,7 @@ void CodeCore1(void * parameter ) {
       //Complex to Magnitude ~2700us
       FFT.ComplexToMagnitude(Real, Imag, samples / 2);
 
-      Real[0] = 0;
+      Real[0] = Real[1];
     }
     //Writing Data to Buffer
     for (uint16_t i = 0; i < samples / 2; i++) {
@@ -250,22 +175,21 @@ void CodeCore1(void * parameter ) {
     }
 
     //Delaying next Cycle to match fps setting
-    TimingNew[1] = micros();
-    while (TimingNew[1] - TimingOld[1] < CorePeriod) {
-      TimingNew[1] = micros();
+    TimingNew = micros();
+    while (TimingNew - TimingOld < CorePeriod) {
+      TimingNew = micros();
     }
-    checker[6] = 0;
+    checker = 0;
   }
 }
 
 
 void startsampling() {
-  for (uint16_t i = 0; i < samples; i++)
-  {
-    oldTime = micros();
+  for (uint16_t i = 0; i < samples; i++) {
 
-    u[i] = analogRead(Audio);
-    vReal[i] = int8_t((amplitude * (u[i]) / 4096) / 2.0);
+    oldTime = micros();
+    u = analogRead(Audio);
+    vReal[i] = int8_t((amplitude * u / 4096) / 2.0);
 
     vRealBuffer[i] = vReal[i];
     if (i < LED_COUNT) {
@@ -281,6 +205,7 @@ void startsampling() {
 
 void startdrawing() {
 
+  float R = 0, G = 0, B = 0;
 
   for (uint8_t Position = 0; Position < LED_COUNT; Position++) {
 
@@ -298,13 +223,10 @@ void startdrawing() {
       B = 255;
     }
 
-
     leds[LED_COUNT - 1 - Position] = CRGB(R, G, B);
-
   }
 
   FastLED.show();
-
 }
 
 void startbuckets() {
@@ -323,6 +245,9 @@ void startbuckets() {
 
     Buckets[i] = Buckets[i] / Bucketentries[i];
     Buckets[i] = 100 * Buckets[i] / BucketAmplitude[i];
+    /*if (Buckets[i] > 10) {
+      Buckets[i] = 100 * (log10(Buckets[i]) - 1);
+      }*/
     if (Buckets[i] > 100) {
       Buckets[i] = 100;
     }
@@ -330,35 +255,141 @@ void startbuckets() {
   }
 
 
-  /*
-    for ( int i = 0; i < LED_COUNT - 1 ; i++) {
-    if (Buckets[i] < Buckets[i + 1]) {
-      Buckets[i] = (Buckets[i] + Buckets[i + 1]) / 2;
-    }
-    }
-    for ( int i = LED_COUNT; i > 0 ; i--) {
-    if (Buckets[i] < Buckets[i - 1]) {
-      Buckets[i] = (Buckets[i] + Buckets[i - 1]) / 2;
-    }
-    }
-  */
   for ( int i = 0; i < LED_COUNT; i++) {
     if (Buckets[i] < BucketsOld[i]) {
       Buckets[i] = (Buckets[i] + 2 * BucketsOld[i]) / 3;
     }
   }
-  bool reset = true;
-  for (uint16_t i = 0; i < 4; i++) {
-    if (Buckets[i] > 90) {
-      Basscount++;
-      reset = false;
+}
+
+void BucketFrequencyFunction() {
+
+#define Zeile 6
+#define Spalte 7
+  double Matrix[Zeile][Spalte] = { {1, 1, 1, 1, 1, 1, 39},
+    {pow(59, 5), pow(59, 4), pow(59, 3), pow(59, 2), 59, 1, 12000},
+    {pow(33, 5), pow(33, 4),  pow(33, 3), pow(33, 2), 33, 1,  4000},
+    {pow(50, 5), pow(50, 4), pow(50, 3), pow(50, 2), 50, 1,  8000},
+    {pow(64, 5), pow(64, 4), pow(64, 3), pow(64, 2), 64, 1, 16000},
+    {80, 32, 12, 4, 1, 0, 39}
+  };
+  double a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
+  float x = 0;
+
+  for (int k = 0; k < Zeile; k++) {
+    for (int j = k + 1; j < Zeile; j++) {
+      a = Matrix[j][k] / Matrix[k][k];
+      for (int i = k; i < Spalte; i++) {
+        Matrix[j][i] = Matrix[j][i] - a * Matrix[k][i];
+      }
+    }
+    for (int ii = 0; ii < Zeile; ii++) {
+      for (int jj = 0; jj < Spalte; jj++) {
+        Matrix[ii][jj] /= 10;
+      }
     }
   }
-  if (reset) {
-    Basscount = 0;
+
+  f = Matrix[5][6] / Matrix[5][5];
+  e = (Matrix[4][6] - Matrix[4][5] * f) / Matrix[4][4];
+  d = (Matrix[3][6] - Matrix[3][5] * f - Matrix[3][4] * e ) / Matrix[3][3];
+  c = (Matrix[2][6] - Matrix[2][5] * f - Matrix[2][4] * e - Matrix[2][3] * d) / Matrix[2][2];
+  b = (Matrix[1][6] - Matrix[1][5] * f - Matrix[1][4] * e - Matrix[1][3] * d - Matrix[1][2] * c) / Matrix[1][1];
+  a = (Matrix[0][6] - Matrix[0][5] * f - Matrix[0][4] * e - Matrix[0][3] * d - Matrix[0][2] * c - Matrix[0][1] * b) / Matrix[0][0];
+
+  for (int i = 1; i <= LED_COUNT; i++) {
+    BucketFrequency[i - 1] = (a * pow(i, 5)) + (b * pow(i, 4)) + (c * pow(i, 3)) + (d * pow(i, 2)) + (e * i) + f;
   }
 }
 
+void BucketFrequencyManual() {
+
+  int startingFrequency = lowestband;
+  int delta = 7;
+
+  for (int i = 0; i < LED_COUNT; i++) {
+
+    if (i == 0) {
+      BucketFrequency[i] = startingFrequency;
+    }
+    else {
+      BucketFrequency[i] = BucketFrequency[i - 1] + startingFrequency;
+    }
+    startingFrequency = startingFrequency + delta;
+  }
+}
+
+void vRealtoBucket() {
+
+  for (uint16_t i = 0; i < samples / 2; i++) {
+
+    int Diff[LED_COUNT];
+    int smallest;
+    double abscissa = ((i * 1.0 * samplingFrequency) / samples);
+    for (int j = 0; j < LED_COUNT; j++) {
+      Diff[j] = abscissa - BucketFrequency[j];
+
+      if (Diff[j] < 0) {
+        Diff[j] = -Diff[j];
+      }
+
+      if (j == 0) {
+        smallest = Diff[j];
+      }
+      if (smallest >= Diff[j]) {
+        smallest = Diff[j];
+        smallestPosition[i] = j;
+
+      }
+    }
+  }
+
+  for (int n = 0; n < LED_COUNT; n++) {
+    for (int i = 0; i < samples / 2; i++) {
+      if (smallestPosition[i] == n) {
+        Bucketentries[n]++;
+      }
+    }
+  }
+}
+
+void LEDColor() {
+
+  int quarter = BucketFrequency[LED_COUNT - 1] / 4;
+  int sub[3];
+  for (int i = 0; i < LED_COUNT; i++) {
+    if (BucketFrequency[i] <= quarter) {
+      Rfix[i] = 255;
+      Gfix[i] = 255 * BucketFrequency[i] / quarter;
+      Bfix[i] = 0;
+      sub[0] = BucketFrequency[i];
+    }
+    else if (BucketFrequency[i] <= 2 * quarter && BucketFrequency[i] > quarter) {
+      Rfix[i] = 255 - 255 * (BucketFrequency[i] - sub[0]) / quarter;
+      Gfix[i] = 255;
+      Bfix[i] = 0;
+      sub[1] = BucketFrequency[i];
+    }
+    else if (BucketFrequency[i] <= 3 * quarter && BucketFrequency[i] > 2 * quarter) {
+      Rfix[i] = 0;
+      Gfix[i] = 255;
+      Bfix[i] = 255 * (BucketFrequency[i] - sub[1]) / quarter;
+      sub[2] = BucketFrequency[i];
+    }
+    else {
+      Rfix[i] = 0;
+      Gfix[i] = 255 - 255 * (BucketFrequency[i] - sub[2]) / quarter;
+      Bfix[i] = 255;
+    }
+    if (Rfix[i] < 0) {
+      Rfix[i] = 0;
+    } if (Gfix[i] < 0) {
+      Gfix[i] = 0;
+    } if (Bfix[i] < 0) {
+      Bfix[i] = 0;
+    }
+  }
+}
 
 void loop()
 {
